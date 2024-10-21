1c4ab6c24886b1b4206037ba54a4537c0e3c9676
Likhon Sheikh <likhondocs@gmail.com>
2024-10-21 21:57:22 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Using CountAPI to track total page visits
-    function cb(response) {
-        document.getElementById('visits').innerText = response.value;
-    }
-
-    // Fix the player resizing issue when minimizing/maximizing
-    const playerWrapper = document.querySelector('.player-wrapper');
-    const resizePlayer = () => {
-        if (playerWrapper) {
-            playerWrapper.style.height = `${playerWrapper.offsetWidth * 9 / 16}px`;
-        }
-    };
-    window.addEventListener('resize', resizePlayer);
-    resizePlayer(); // Initial call
-});
+        // Fix the player resizing issue when minimizing/maximizing
+        const playerWrapper = document.querySelector('.player-wrapper');
+        const resizePlayer = () => {
+            if (playerWrapper) {
+                playerWrapper.style.height = `${playerWrapper.offsetWidth * 9 / 16}px`;
+            }
+        };
+        window.addEventListener('resize', resizePlayer);
+        resizePlayer(); // Initial call
